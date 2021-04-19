9491fb69d76f131804de1224f126ae2675e2c571
Joelson Junior <joelson.ejr@gmail.com>
2021-04-19 20:12:47 -0300

    nlw5 initial commint

---
+++ podcastr/classNotes.js
@@ -0,0 +1,29 @@
+/* 
+
+Componente - uma função que retorna HTML. Eles facilitam tanto na manutenção do
+  código, quanto na reutilização do mesmo. Tudo dentro do react é um componente. 
+
+Propriedade - é uma informação que é repassada de um componente para outro.
+
+Um elemento não pode ser colocado em baixo do outro, sem que tenha algo em volta
+(como uma div). T
+
+<div>
+  <Button/>
+  <Button/>
+  <Button/>
+  <Button/>
+</div>
+
+Para não ter de utilizar uma 'div' exclusivamente para isso, pode ser utilizado
+um 'fragment'
+
+<> //fragment 
+  <Button/>
+  <Button/>
+  <Button/>
+  <Button/>
+</> 
+
+
+*/
